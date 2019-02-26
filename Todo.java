93f4f399185db6e9d43a233718879eeffb468641
Jordan Lanoue <rlanoue@ou.edu>
2019-02-25 19:21:41 -0600

    Pushed new files and three completed classes

---
+++ Todo.java
@@ -0,0 +1,38 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-25
+OU ID_______________: 113475042
+OU EMAIL____________: rlanoue@ou.edu
+
+Tasks:
+1. finish constructor Trapezoid
+2. finish getShapeType Trapezoid  
+3. finish constructor Triangle
+4. finish getArea Triangle
+5. finish getShapeType
+6. finish getPerimeter Polygon
+7. finish compareTo Shape Shape
+8. finish toString Shape
+
+Estimated Time (minutes):
+1. 5
+2. 10
+3. 5
+4. 10
+5. 10
+6. 10
+7. 15
+8. 10
+
+
+Actual Time (minutes):
+1. 5
+2. 1
+3. 2
+4. 2
+5. 1
+6. 7
+7. 
+
+
+*/}
